2a863a82bb90ce0df0c2c3bc1f8a59eacc7b4c53
Cankut <kostur15@itu.edu.tr>
2020-09-30 12:10:10 +0300

    Add support action bar

--- app/src/main/java/com/example/weatherapp/MainActivity.kt
+++ app/src/main/java/com/example/weatherapp/MainActivity.kt
@@ -2,10 +2,26 @@ package com.example.weatherapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import androidx.navigation.findNavController
+import androidx.navigation.ui.NavigationUI
+import androidx.navigation.ui.setupActionBarWithNavController
+import com.example.weatherapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        val binding = DataBindingUtil.setContentView<ActivityMainBinding>(this, R.layout.activity_main)
+
+        val navController = findNavController(R.id.nav_host_fragment)
+
+        setSupportActionBar(binding.toolbar)
+        setupActionBarWithNavController(navController)
+
     }
+
+    override fun onSupportNavigateUp():Boolean{
+        return findNavController(R.id.nav_host_fragment).navigateUp()
+    }
+
 }
