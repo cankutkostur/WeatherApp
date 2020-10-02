639f93a47d0199ee6989b3f495103f328e713431
Cankut <kostur15@itu.edu.tr>
2020-10-02 10:34:29 +0300

    Add factory for add view model

--- app/src/main/java/com/example/weatherapp/ui/add/AddViewModelFactory.kt
+++ app/src/main/java/com/example/weatherapp/ui/add/AddViewModelFactory.kt
@@ -1,2 +1,17 @@
 package com.example.weatherapp.ui.add
 
+import android.app.Application
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+
+class AddViewModelFactory(
+    private val application: Application
+) : ViewModelProvider.Factory {
+    @Suppress("unchecked_cast")
+    override fun <T : ViewModel?> create(modelClass: Class<T>): T {
+        if (modelClass.isAssignableFrom(AddViewModel::class.java)) {
+            return AddViewModel(application) as T
+        }
+        throw IllegalArgumentException("Unknown ViewModel class")
+    }
+}
