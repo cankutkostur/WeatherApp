43323ff643b8a5a4c0cff801a8ef0ccbc5e3f08c
Cankut <kostur15@itu.edu.tr>
2020-10-02 17:58:18 +0300

    Update list view model

--- app/src/main/java/com/example/weatherapp/ui/list/ListViewModel.kt
+++ app/src/main/java/com/example/weatherapp/ui/list/ListViewModel.kt
@@ -1,23 +1,50 @@
 package com.example.weatherapp.ui.list
 
+import android.app.Application
+import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.example.weatherapp.database.getDatabase
+import com.example.weatherapp.domain.DomainCity
 
-class ListViewModel : ViewModel() {
+
+class ListViewModel(app: Application) : AndroidViewModel(app) {
+    val dao = getDatabase(app).weatherDao
+
+    val favCities = dao.getAllCities()
+
     private var _navigateToAdd = MutableLiveData<Boolean>()
     val navigateToAdd: LiveData<Boolean>
         get() = _navigateToAdd
 
+    //TODO change type int to city class
+    private var _selectedCities = MutableLiveData<MutableList<Int>>()
+    val selectedCities: LiveData<MutableList<Int>>
+        get() = _selectedCities
+
     init {
         _navigateToAdd.value = false
+        _selectedCities.value = mutableListOf<Int>()
     }
 
-    fun addCity(){
+    fun addCity() {
         _navigateToAdd.value = true
     }
 
-    fun addCityCompleted(){
+    fun addCityCompleted() {
         _navigateToAdd.value = false
+    }
+
+    fun onSelect(city: Int) {
+        _selectedCities.value!!.add(city)
+    }
+
+    fun onCityClicked(city: DomainCity) {
+        //TODO implement
+    }
+
+    fun onCityNavigated() {
+        //TODO implement
     }
 }
