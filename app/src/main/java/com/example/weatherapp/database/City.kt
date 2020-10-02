3ed5ab3f232c90e67bfb44fd3fe78867a9cc3003
Cankut <kostur15@itu.edu.tr>
2020-10-02 10:31:04 +0300

    Add domain city class

--- app/src/main/java/com/example/weatherapp/database/City.kt
+++
@@ -1,9 +0,0 @@
-package com.example.weatherapp.domain
-
-class City(
-    val id: Long,
-    val name: String,
-    val country: String,
-    val lon: Double,
-    val lat: Double
-)
---
+++ app/src/main/java/com/example/weatherapp/domain/DomainCity.kt
@@ -0,0 +1,15 @@
+package com.example.weatherapp.domain
+
+import androidx.room.Embedded
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+class City(
+    val id: Long,
+    val name: String,
+    val country: String,
+    val coord: Coords
+)
+
+data class Coords(val lon: Double,
+                  val lat: Double)
