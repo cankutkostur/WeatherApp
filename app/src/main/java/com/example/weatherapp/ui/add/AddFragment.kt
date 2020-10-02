d41c7e4c91795058309bd7df5a2f57df24ec0df1
Cankut <kostur15@itu.edu.tr>
2020-10-02 10:33:38 +0300

    Add text watcher for search

--- app/src/main/java/com/example/weatherapp/ui/add/AddFragment.kt
+++ app/src/main/java/com/example/weatherapp/ui/add/AddFragment.kt
@@ -2,14 +2,23 @@ package com.example.weatherapp.ui.add
 
 import android.content.Context
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.view.inputmethod.InputMethodManager
 import androidx.core.content.ContextCompat.getSystemService
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import com.example.weatherapp.databinding.FragmentAddBinding
+import com.squareup.moshi.JsonAdapter
+import com.squareup.moshi.JsonClass
+import com.squareup.moshi.Moshi
+import com.squareup.moshi.Types
+import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
+
 
 class AddFragment : Fragment() {
 
@@ -33,12 +42,35 @@ class AddFragment : Fragment() {
         // set focus to city name text
         binding.cityName.requestFocus()
 
+        val adapter = AddAdapter()
+
+        binding.cityList.adapter = adapter
+
+        viewModel.cities.observe(viewLifecycleOwner, Observer {
+            it?.let {
+                adapter.submitList(it)
+            }
+        })
+
         // show the keyboard
         val imm = activity?.getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
         imm.showSoftInput(binding.cityName, 0)
 
+        // change listener for search editText view
+        binding.cityName.addTextChangedListener(object : TextWatcher{
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+            }
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                viewModel.showCities(s.toString())
+            }
+
+            override fun afterTextChanged(s: Editable?) {
+            }
+
+        })
+
         return binding.root
     }
-
 
 }
