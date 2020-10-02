f374ecf5534f80718fc70f73fca259b11a9d7d04
Cankut <kostur15@itu.edu.tr>
2020-10-02 17:42:10 +0300

    Add weather database

--- app/src/main/java/com/example/weatherapp/database/WeatherDatabase.kt
+++ app/src/main/java/com/example/weatherapp/database/WeatherDatabase.kt
@@ -1,2 +1,28 @@
 package com.example.weatherapp.database
 
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+import com.example.weatherapp.database.models.DatabaseCity
+
+@Database(entities = [DatabaseCity::class], version = 1, exportSchema = false)
+abstract class WeatherDatabase : RoomDatabase() {
+    abstract val weatherDao: WeatherDao
+}
+
+@Volatile
+private lateinit var INSTANCE: WeatherDatabase
+
+fun getDatabase(context: Context): WeatherDatabase {
+    synchronized(WeatherDatabase::class.java) {
+        if (!::INSTANCE.isInitialized) {
+            INSTANCE = Room.databaseBuilder(context.applicationContext,
+                WeatherDatabase::class.java,
+                "weather_database")
+                .fallbackToDestructiveMigration()
+                .build()
+        }
+    }
+    return INSTANCE
+}
