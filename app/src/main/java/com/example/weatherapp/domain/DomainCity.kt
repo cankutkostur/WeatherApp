5dc7f8394389c0d1839732387d7512ce51478117
Cankut <kostur15@itu.edu.tr>
2020-10-02 17:42:25 +0300

    Add domain city class

--- app/src/main/java/com/example/weatherapp/domain/DomainCity.kt
+++ app/src/main/java/com/example/weatherapp/domain/DomainCity.kt
@@ -3,13 +3,22 @@ package com.example.weatherapp.domain
 import androidx.room.Embedded
 import androidx.room.Entity
 import androidx.room.PrimaryKey
+import com.example.weatherapp.database.models.Coords
+import com.example.weatherapp.database.models.DatabaseCity
 
 class DomainCity(
     val id: Long,
     val name: String,
     val country: String,
-    val coord: Coords
+    val coord: Coords,
+    var isSelected: Boolean = false
 )
 
-data class Coords(val lon: Double,
-                  val lat: Double)
+fun DomainCity.asDatabaseModel(): DatabaseCity{
+    return DatabaseCity(
+        id,
+        name,
+        country,
+        coord
+    )
+}
