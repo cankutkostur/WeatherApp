5e7ee9438cfb4fd6faf73c95a34d8cee3a9aff8e
Cankut <kostur15@itu.edu.tr>
2020-10-02 17:58:33 +0300

    Add set country name to bindings

--- app/src/main/java/com/example/weatherapp/Util/BindingUtil.kt
+++ app/src/main/java/com/example/weatherapp/Util/BindingUtil.kt
@@ -4,9 +4,16 @@ import android.widget.TextView
 import androidx.databinding.BindingAdapter
 import com.example.weatherapp.domain.DomainCity
 
-@BindingAdapter("cityName")
-fun TextView.setSleepDurationFormatted(item: DomainCity?) {
+@BindingAdapter("setCityName")
+fun TextView.setCityName(item: DomainCity?) {
     item?.let {
         text = item.name
+    }
+}
+
+@BindingAdapter("setCountryName")
+fun TextView.setCountryName(item: DomainCity?) {
+    item?.let {
+        text = "(" + item.country + ")"
     }
 }
