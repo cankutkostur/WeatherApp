a2ecaff51d0114231ff0091e794fcf9b992202a5
Cankut <kostur15@itu.edu.tr>
2020-10-07 11:50:46 +0300

    Update list item adapter to use new data class

--- app/src/main/java/com/example/weatherapp/ui/list/ListItemAdapter.kt
+++ app/src/main/java/com/example/weatherapp/ui/list/ListItemAdapter.kt
@@ -9,26 +9,29 @@ import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
 import com.example.weatherapp.databinding.ListItemBinding
 import com.example.weatherapp.domain.DomainCity
+import com.example.weatherapp.domain.DomainCityWithHourlyAndDaily
 
 
-class ListItemAdapter(val clickListener: ListItemListener) : ListAdapter<DomainCity, ListItemAdapter.ViewHolder>(CityDiffCallback()) {
+class ListItemAdapter(val clickListener: ListItemListener) : ListAdapter<DomainCityWithHourlyAndDaily, ListItemAdapter.ViewHolder>(CityDiffCallback()) {
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
-        holder.bind(getItem(position)!!)
+        val item = getItem(position)
+        holder.itemView.setOnClickListener{
+            clickListener.onClick(item)
+        }
+        holder.itemView.setOnLongClickListener{
+            clickListener.onLongClick(item)
+        }
+        holder.bind(item)
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
         return ViewHolder.from(parent, clickListener)
     }
 
-    class ViewHolder private constructor(val binding: ListItemBinding, val clickListener: ListItemListener) : RecyclerView.ViewHolder(binding.root),
-        View.OnLongClickListener {
-        init {
-            binding.listItem.setOnLongClickListener(this)
-        }
-
-        fun bind(item: DomainCity) {
-            binding.city = item
+    class ViewHolder private constructor(val binding: ListItemBinding, val clickListener: ListItemListener) : RecyclerView.ViewHolder(binding.root) {
+        fun bind(item: DomainCityWithHourlyAndDaily) {
+            binding.item = item
             binding.clickListener = clickListener
             binding.executePendingBindings()
         }
@@ -41,28 +44,23 @@ class ListItemAdapter(val clickListener: ListItemListener) : ListAdapter<DomainC
             }
         }
 
-        override fun onLongClick(view: View?): Boolean {
-            clickListener.onLongClick(binding.city!!)
-            return true
-        }
-
     }
 }
 
 
-class CityDiffCallback : DiffUtil.ItemCallback<DomainCity>() {
+class CityDiffCallback : DiffUtil.ItemCallback<DomainCityWithHourlyAndDaily>() {
 
-    override fun areItemsTheSame(oldItem: DomainCity, newItem: DomainCity): Boolean {
-        return oldItem.id == newItem.id
+    override fun areItemsTheSame(oldItem: DomainCityWithHourlyAndDaily, newItem: DomainCityWithHourlyAndDaily): Boolean {
+        return oldItem.city.id == newItem.city.id
     }
 
-    @SuppressLint("DiffUtilEquals")
-    override fun areContentsTheSame(oldItem: DomainCity, newItem: DomainCity): Boolean {
+    override fun areContentsTheSame(oldItem: DomainCityWithHourlyAndDaily, newItem: DomainCityWithHourlyAndDaily): Boolean {
         return oldItem == newItem
     }
 }
 
-class ListItemListener(val clickListener: (city: DomainCity) -> Unit, val longClickListener : (city: DomainCity) -> Unit) {
-    fun onClick(city: DomainCity) = clickListener(city)
-    fun onLongClick(city: DomainCity) = longClickListener(city)
+class ListItemListener(val clickListener: (city: DomainCityWithHourlyAndDaily) -> Unit,
+                       val longClickListener : (city: DomainCityWithHourlyAndDaily) -> Boolean) {
+    fun onClick(city: DomainCityWithHourlyAndDaily) = clickListener(city)
+    fun onLongClick(city: DomainCityWithHourlyAndDaily) = longClickListener(city)
 }
