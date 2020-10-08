b3dc2ccd25bdb131dba8cdf6dba3d7c9ec8a85cb
Cankut <kostur15@itu.edu.tr>
2020-10-08 17:55:32 +0300

    Add detail viewmodel

--- app/src/main/java/com/example/weatherapp/ui/detail/DetailViewModel.kt
+++ app/src/main/java/com/example/weatherapp/ui/detail/DetailViewModel.kt
@@ -1,7 +1,20 @@
 package com.example.weatherapp.ui.detail
 
-import androidx.lifecycle.ViewModel
+import android.app.Application
+import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import com.example.weatherapp.domain.DomainCityWithHourlyAndDaily
+import com.example.weatherapp.domain.DomainHourly
 
-class DetailViewModel : ViewModel() {
+class DetailViewModel(city: DomainCityWithHourlyAndDaily, app: Application) : AndroidViewModel(app) {
 
+    private val _selectedCity = MutableLiveData<DomainCityWithHourlyAndDaily>()
+    val selectedCity: LiveData<DomainCityWithHourlyAndDaily>
+        get() = _selectedCity
+
+    init {
+        _selectedCity.value = city
+
+    }
 }
