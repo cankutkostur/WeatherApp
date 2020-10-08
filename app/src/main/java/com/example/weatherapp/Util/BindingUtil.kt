bf413b4a5e6bec690b2f4bae36c2d5f13d138d91
Cankut <kostur15@itu.edu.tr>
2020-10-08 17:56:14 +0300

    Add binding utils

--- app/src/main/java/com/example/weatherapp/Util/BindingUtil.kt
+++ app/src/main/java/com/example/weatherapp/Util/BindingUtil.kt
@@ -5,6 +5,10 @@ import android.widget.TextView
 import androidx.databinding.BindingAdapter
 import com.example.weatherapp.R
 import com.example.weatherapp.domain.DomainCityWithHourlyAndDaily
+import com.example.weatherapp.domain.DomainWeather
+import com.example.weatherapp.domain.Temp
+import java.time.LocalTime
+import java.util.*
 
 @BindingAdapter("cityName")
 fun TextView.setCityName(item: String) {
@@ -21,17 +25,26 @@ fun TextView.setCountryName(item: String?) {
 }
 
 @BindingAdapter("temperature")
-fun TextView.setTemperature(item: DomainCityWithHourlyAndDaily?) {
+fun TextView.setTemperature(item: Double) {
     item?.let {
-        item.city.current?.temp?.let {
-            text = resources.getString(R.string.display_temperature, item.city.current.temp)
+        item?.let {
+            text = resources.getString(R.string.display_temperature, it)
+        }
+    }
+}
+
+@BindingAdapter("weatherText")
+fun TextView.setWeatherText(item: DomainCityWithHourlyAndDaily?) {
+    item?.let {
+        item.city.current?.weather?.description?.let {
+            text = it
         }
     }
 }
 
 @BindingAdapter("weatherImage")
-fun ImageView.setWeatherImage(item: DomainCityWithHourlyAndDaily?) {
-    item?.city?.current?.weather?.icon?.let {
+fun ImageView.setWeatherImage(item: DomainWeather?) {
+    item?.icon?.let {
         setImageResource(when (it.dropLast(1)) {
             "01" -> R.drawable.ic_sunny
             "02" -> R.drawable.ic_sunny_intervals
@@ -43,5 +56,61 @@ fun ImageView.setWeatherImage(item: DomainCityWithHourlyAndDaily?) {
             "13" -> R.drawable.ic_cloudy_with_heavy_snow
             else -> R.drawable.ic_mist
         })
+    }
+}
+
+@BindingAdapter("day")
+fun TextView.setDay(item: Long?) {
+    item?.let {
+        text = convertLongToDateString(item * 1000).substringBefore(' ')
+    }
+}
+
+@BindingAdapter("hour")
+fun TextView.setHour(item: Long?) {
+    item?.let {
+        text = convertLongToDateString(item * 1000, ).takeLast(5)
+    }
+}
+
+@BindingAdapter("pressure")
+fun TextView.setPressure(item: Int?) {
+    item?.let {
+        text = resources.getString(R.string.display_pressure, it)
+    }
+}
+
+@BindingAdapter("percent")
+fun TextView.setPercent(item: Int?) {
+    item?.let {
+        text = resources.getString(R.string.display_percent, it)
+    }
+}
+
+@BindingAdapter("distance")
+fun TextView.setDistance(item: Int?) {
+    item?.let {
+        text = resources.getString(R.string.display_distance, it)
+    }
+}
+
+@BindingAdapter("speed")
+fun TextView.setSpeed(item: Double?) {
+    item?.let {
+        text = resources.getString(R.string.display_speed, it)
+    }
+}
+
+@BindingAdapter("degree")
+fun TextView.setDegree(item: Int?) {
+    item?.let {
+        text = resources.getString(R.string.display_degree, it)
+    }
+}
+
+@BindingAdapter("fullDate")
+fun TextView.setFullDate(item: Long?) {
+    item?.let {
+        text = convertLongToDateString(item * 1000)
     }
 }
