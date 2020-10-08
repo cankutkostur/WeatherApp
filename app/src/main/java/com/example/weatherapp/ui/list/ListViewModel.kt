c3973c6f3185357f4fcdbdd61131efa35b889650
Cankut <kostur15@itu.edu.tr>
2020-10-08 17:56:03 +0300

    Add selecting to list view model

--- app/src/main/java/com/example/weatherapp/ui/list/ListViewModel.kt
+++ app/src/main/java/com/example/weatherapp/ui/list/ListViewModel.kt
@@ -4,10 +4,14 @@ import android.app.Application
 import android.widget.Toast
 import androidx.lifecycle.*
 import com.example.weatherapp.database.getDatabase
+import com.example.weatherapp.database.models.CityWithHourlyAndDaily
 import com.example.weatherapp.domain.DomainCity
 import com.example.weatherapp.domain.DomainCityWithHourlyAndDaily
+import com.example.weatherapp.domain.asDatabaseModel
 import com.example.weatherapp.repository.WeatherRepository
+import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 
 
 class ListViewModel(app: Application) : AndroidViewModel(app) {
@@ -15,32 +19,33 @@ class ListViewModel(app: Application) : AndroidViewModel(app) {
     private val database = getDatabase(app)
     private val weatherRepository = WeatherRepository(database)
 
-    var _favCities = weatherRepository.cities
+    val _favCities = weatherRepository.cities
     val favCities: LiveData<List<DomainCityWithHourlyAndDaily>>
         get() = _favCities
 
-    private var _navigateToAdd = MutableLiveData<Boolean>()
+    private val _navigateToAdd = MutableLiveData<Boolean>()
     val navigateToAdd: LiveData<Boolean>
         get() = _navigateToAdd
 
-    private var _navigateToCity = MutableLiveData<DomainCityWithHourlyAndDaily>()
+    private val _navigateToCity = MutableLiveData<DomainCityWithHourlyAndDaily>()
     val navigateToCity: LiveData<DomainCityWithHourlyAndDaily>
         get() = _navigateToCity
 
-    //TODO selecting and deleting must be revised
-    private var _selectedCities = MutableLiveData<MutableList<DomainCity>>()
-    val selectedCities: LiveData<MutableList<DomainCity>>
+    private var selected = mutableListOf<DomainCity>()
+    private var _selectedCities = MutableLiveData<List<DomainCity>>()
+    val selectedCities: LiveData<List<DomainCity>>
         get() = _selectedCities
 
-    val isSelecting = Transformations.map(selectedCities){
+    val isSelecting = Transformations.map(_selectedCities) {
         it.isNotEmpty()
     }
 
     init {
+        _navigateToAdd.value = false
+        _selectedCities.value = selected
         viewModelScope.launch {
             weatherRepository.refreshCities()
         }
-        _navigateToAdd.value = false
     }
 
     fun addCity() {
@@ -48,18 +53,31 @@ class ListViewModel(app: Application) : AndroidViewModel(app) {
     }
 
     fun addCityCompleted() {
+        selected.removeAll(selected)
+        _selectedCities.value = selected
         _navigateToAdd.value = false
     }
 
     fun onSelect(item: DomainCityWithHourlyAndDaily): Boolean {
-        viewModelScope.launch {
-            weatherRepository.deleteCity(item.city)
+        if (selected.contains(item.city)){
+            selected.remove(item.city)
+            _selectedCities.value = selected
         }
+        else {
+            selected.add(item.city)
+            _selectedCities.value = selected
+        }
+
         return true
     }
 
     fun onCityClicked(item: DomainCityWithHourlyAndDaily) {
-        //TODO implement
+        if (selected.isNotEmpty()){
+            onSelect(item)
+        }
+        else{
+            onCityNavigate(item)
+        }
     }
 
     fun onCityNavigate(item: DomainCityWithHourlyAndDaily){
@@ -67,6 +85,19 @@ class ListViewModel(app: Application) : AndroidViewModel(app) {
     }
 
     fun onCityNavigated() {
+        selected.removeAll(selected)
+        _selectedCities.value = selected
         _navigateToCity.value = null
+    }
+
+    fun onDelete(){
+        viewModelScope.launch {
+            withContext(Dispatchers.IO) {
+                weatherRepository.deleteCities(selected)
+            }
+            selected.removeAll(selected)
+            _selectedCities.value = selected
+            val a = 5
+        }
     }
 }
